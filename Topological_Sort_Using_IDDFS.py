dc7efc3c9867124393f26ed6476b16ccd7e5519b
md-rakib-78 <mdrakib53661@gmail.com>
2025-11-03 21:30:37 +0600

    Correction Topological Order using IDDFS

--- Topological_Sort_Using_IDDFS.py
+++ Topological_Sort_Using_IDDFS.py
@@ -1,57 +1,73 @@
 prev = []
 visit = []
-lst = []
 time = 0
+lt = []
+lst = []
+found = False
 
 def DFS(graph, curr, goal, limit, depth, n):
-    global time, lst
+    global time, lst, found
     time += 1
     print(curr, " ", end="")
 
+    visit[curr] = "g"
+
     if curr == goal:
-        return True
+        found = True
 
     if depth == limit:
-        return False
+        visit[curr] = "b"
+        time += 1
+        lt = [time, curr]
+        lst.append(lt)
+        return
 
-    visit[curr] = "g"
-    for v in range(n):
+    for v in range(n - 1, -1, -1):
         if graph[curr][v] == 1 and visit[v] == "w":
             prev[v] = curr
             DFS(graph, v, goal, limit, depth + 1, n)
-    visit[curr] = "b"
-    time += 1
-    lt = [time, curr]
-    lst.append(lt)
-    return False
 
+    if visit[curr] != "b":
+        visit[curr] = "b"
+        time += 1
+        lt = [time, curr]
+        lst.append(lt)
+    return
 
 def IDS(graph, st, en, max_limit):
     n = len(graph)
-    global prev, visit, lst, time
+    global prev, visit, lt, lst, time, found
 
     for limit in range(max_limit + 1):
         prev = [-1] * n
         visit = ["w"] * n
         lst = []
         time = 0
+        found = False
 
         print("Iterative Depth:", limit)
         print("DFS:", end=" ")
 
-        if DFS(graph, st, en, limit, 0, n):
-            print("\nGoal found!\n")
-            print("Topological Order of Robot Node Traversal:")
-            lst.sort(reverse=True)
-            for l in lst:
-                print(" ", l[1], end=" ")
-            print()
-            return
+        DFS(graph, st, en, limit, 0, n)
+
+        print()
+        if found:
+            print("Goal found!\n")
+            print("\n")
         else:
-            print("\nGoal not found at this depth.\n")
+            print("Goal not found at this depth.\n")
+
+        print("Topological Order:")
+        lst.sort(reverse=True)
+        for l in lst:
+            print(" ", l[1], end=" ")
+        print("\n")
+        print(lst)
+
+        if found:
+            return
 
     print("Goal not found in given depth limit.")
-
 
 matrix = []
 with open("Direct Graph Matrix.txt", "r") as file:
