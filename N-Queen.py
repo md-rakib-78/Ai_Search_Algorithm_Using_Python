70dd2a66b0528d21dd31024f7189de446cf7b63b
Md Rakibul Islam <164800788+md-rakib-78@users.noreply.github.com>
2025-12-09 15:50:12 +0600

    Update N-Queen.py

--- N-Queen.py
+++ N-Queen.py
@@ -1,33 +1,78 @@
 prev = []
 visit = []
-i=0
-j=0
+i = 0
+j = 0
 
 
+def DFS_Visit(graph):
+    global i, j
 
-def DFS_Visit(graph, curr, n):
+    N = len(graph)
 
-    if graph[][]==
+    if i == N:
+        return True
+
+    if j == N:
+        return False
+
+    graph[i][j] = 1
+
+    for row in range(i - 1, -1, -1):
+        if graph[row][j] == 1:
+            graph[i][j] = 0
+            j += 1
+            return DFS_Visit(graph)
+
+    for d in range(1, min(i, j) + 1):
+        if graph[i - d][j - d] == 1:
+            graph[i][j] = 0
+            j += 1
+            return DFS_Visit(graph)
+
+    for d in range(1, min(i, N - j - 1) + 1):
+        if graph[i - d][j + d] == 1:
+            graph[i][j] = 0
+            j += 1
+            return DFS_Visit(graph)
 
 
+    ni = i + 1
+    nj = 0
+
+
+    old_i, old_j = i, j
+
+    i, j = ni, nj
+    if DFS_Visit(graph):
+        return True
+
+    i, j = old_i, old_j
+    graph[i][j] = 0
+    j += 1
+    return DFS_Visit(graph)
 
 
 
 def DFS(graph):
-    global visit,i,j
+    global visit, i, j
     visit = ["w"] * len(graph)
-    DFS_Visit(graph, 0, len(graph))
+    i = 0
+    j = 0
+    DFS_Visit(graph)
+
 
 
 matrix = []
-numQueen=int(input("Enter the number of Queens: "))
+numQueen = int(input("Enter number of Queens: "))
+
 for i in range(numQueen):
-    lst=[]
+    lst = []
     for j in range(numQueen):
         lst.append(0)
     matrix.append(lst)
 
-print(matrix)
-
-print()
 DFS(matrix)
+
+print("\nFinal N-Queen Solution:")
+for r in matrix:
+    print(r)
